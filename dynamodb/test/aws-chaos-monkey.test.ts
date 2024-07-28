8cf1a6a281264bcc0335949f98a461e5b3cb80da
max <maxcheung@optusnet.com.au>
2024-07-28 16:46:55 +1000

    update dynamodb readme

---
+++ dynamodb/test/aws-chaos-monkey.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsChaosMonkey from '../lib/aws-chaos-monkey-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-chaos-monkey-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsChaosMonkey.AwsChaosMonkeyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
